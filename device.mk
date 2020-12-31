79f5e4ed2882546b7dc20bea126cb61e9b0e9392
goodmeow <harunbam3@gmail.com>
2020-12-31 09:51:14 +0700

    cleanup unused overlay & package

--- device.mk
+++ device.mk
@@ -25,10 +25,6 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
     $(LOCAL_PATH)/overlay-lineage
 
-PRODUCT_PACKAGES += \
-    NoCutoutOverlay \
-    NusantaraPapers
-
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
     $(LOCAL_PATH)/overlay-lineage/lineage-sdk
 
